d57d163a1e3f7390d5accc31889fba5721f05560
Choring <skyhonor201@gmail.com>
2022-04-25 13:53:10 +0900

    서버첫구동

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("hello world")
+var express = require('express')
+var app = express() //app 객체
 
-var a = 3;
+//맨 밑에 놓기
+app.get('/hello', (req,res) => {
+    res.send('hello world');
+})
+app.listen(5000, ()=> {
+    console.log('server is running on port 5000 !!!')
+})
